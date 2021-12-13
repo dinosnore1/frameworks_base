50c0da5549cd178439478887788cdfdbc651c98f
Pranav Vashi <neobuddy89@gmail.com>
2021-12-13 08:34:09 +0530

    Battery Styles: Add customization for QS
    * Squashed and forwarded ported from A11.
    
    Author: Pranav Vashi <neobuddy89@gmail.com>
    Date:   Sat Mar 26 09:59:36 2022 +0530
    
        base: Remove QS battery location settings
    
    [palaych: Remove battery estimate switcher]
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: palaych <palaych@arrowos.net>
    Change-Id: Ic2202e6c4ca0d076f996d4a4d031cbd202efd1bc

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5751,6 +5751,21 @@ public final class Settings {
         public static final String BACK_GESTURE_HAPTIC = "back_gesture_haptic";
 
         /**
+         * QS battery style
+         * @hide
+         */
+        public static final String QS_BATTERY_STYLE = "qs_battery_style";
+
+        /**
+         * QS battery %
+         * 0: Hide the battery percentage
+         * 1: Display the battery percentage inside the icon
+         * 2: Display the battery percentage next to the icon
+         * @hide
+         */
+        public static final String QS_SHOW_BATTERY_PERCENT = "qs_show_battery_percent";
+
+        /**
          * Battery style
          * @hide
          */
